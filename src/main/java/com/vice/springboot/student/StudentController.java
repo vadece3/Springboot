b4a7d7c7b66ba9a7ca229f9ebc654be70eec80d9
KAMDEM VADECE <vadece3@gmail.com>
2024-09-22 19:01:26 +0200

    Initial commit

---
+++ src/main/java/com/vice/springboot/student/StudentController.java
@@ -0,0 +1,39 @@
+package com.vice.springboot.student;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+
+public class StudentController {
+//	@GetMapping
+//	public String hello() {
+//		return "HELLO World";
+//	}
+//	
+	private final StudentService studentService;
+	
+	@Autowired
+	public StudentController(StudentService studentService) {
+		this.studentService = studentService;
+	}
+	
+	@RequestMapping(path = "api/v1/student1")
+	@GetMapping
+	public List<Student> getStudent1() {
+		return studentService.getStudent1();
+	}
+	
+	@RequestMapping(path = "api/v1/student2")
+	@GetMapping
+	public List<Student> getStudent2() {
+		return studentService.getStudent2();
+	}
+
+
+}
---
+++ src/main/java/com/vice/springboot/student/StudentService.java
@@ -0,0 +1,36 @@
+package com.vice.springboot.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+	
+	private final StudentRepository studentRepository;
+	
+	@Autowired
+	public StudentService(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
+	
+	public List<Student> getStudent1() {
+		return studentRepository.findAll();
+	}
+	
+	public List<Student> getStudent2() {
+		return List.of(
+				new Student(
+						1L,
+						"Mariam",
+						"mariam@gmail.com",
+						LocalDate.of(2000, Month.JANUARY, 5),
+						22
+						)
+				);
+	}
+
+}
