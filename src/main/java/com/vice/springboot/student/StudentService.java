4e961ca2e3a8e31744d2fcff58be529a4afaea1f
KAMDEM VADECE <vadece3@gmail.com>
2024-09-23 13:06:01 +0200

    implementing DELETE

--- src/main/java/com/vice/springboot/student/StudentService.java
+++ src/main/java/com/vice/springboot/student/StudentService.java
@@ -43,4 +43,14 @@ public class StudentService {
 		studentRepository.save(student);
 	}
 
+	public void deleteStudent(Long studentId) {
+		boolean exists = studentRepository.existsById(studentId);
+		if (!exists) {
+			throw new IllegalStateException(
+					"student with id " + studentId + " does not exists");
+		} else {
+			studentRepository.deleteById(studentId);
+		}
+	}
+
 }
