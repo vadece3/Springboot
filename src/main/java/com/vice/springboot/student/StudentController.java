7122bbaf623551b168887b2ed034092dc6ffde44
KAMDEM VADECE <vadece3@gmail.com>
2024-09-24 06:15:47 +0200

    implementing PUT

--- src/main/java/com/vice/springboot/student/StudentController.java
+++ src/main/java/com/vice/springboot/student/StudentController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -55,6 +57,24 @@ public class StudentController {
 	@DeleteMapping
 	public void deleteStudent1(@RequestBody Long studentId) {
 		studentService.deleteStudent(studentId);
+	}
+	
+	@RequestMapping(path = "api/v1/updateStudent/{studentId}")
+	@PutMapping
+	public void updateStudent1(
+			@PathVariable("studentId") Long studentId,
+			@RequestParam(required = false) String name,
+			@RequestParam(required = false) String email) {
+		studentService.updateStudent(studentId, name, email);
+	}
+	
+	@RequestMapping(path = "api/v1/updateStudent")
+	@PutMapping
+	public void updateStudent2(
+			@RequestBody Long studentId,
+			@RequestParam(required = false) String name,
+			@RequestParam(required = false) String email) {
+		studentService.updateStudent(studentId, name, email);
 	}
 
 }
--- src/main/java/com/vice/springboot/student/StudentService.java
+++ src/main/java/com/vice/springboot/student/StudentService.java
@@ -3,10 +3,12 @@ package com.vice.springboot.student;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StudentService {
@@ -50,6 +52,33 @@ public class StudentService {
 					"student with id " + studentId + " does not exists");
 		} else {
 			studentRepository.deleteById(studentId);
+		}
+	}
+
+	@Transactional
+	public void updateStudent(
+			Long studentId, 
+			String name, 
+			String email) {
+		
+		Student student = studentRepository.findById(studentId)
+				.orElseThrow(() -> new IllegalStateException(
+						"student with id " + studentId + " does not exist"));
+		if (name != null &&
+				name.length() > 0 &&
+				!Objects.equals(student.getName(), name)) {
+			student.setName(name);
+		}
+		
+		if (email != null &&
+				email.length() > 0 &&
+				!Objects.equals(student.getEmail(), email)) {
+			Optional<Student> studentOptional = studentRepository
+					.findStudentByEmail(email);
+			if (studentOptional.isPresent()) {
+				throw new IllegalStateException("email taken");
+			}
+			student.setEmail(email);
 		}
 	}
 
